12f4aac27b5af7a133f700f8ca0b3c4a0818111b
G. M Biggan <gmbiggan@gmail.com>
2026-03-16 01:32:54 +0530

    Variable and Data type

---
+++ Assignment_2.2.java
@@ -0,0 +1,14 @@
+/*
+ * Assignment-2.2 (Variable and Data type)
+ * Topic: Product details of a Smart Watch
+ * Step 1: Create a class called "SmartWatch"
+ * Step 2: Create a "main" method
+ * Step 3: Declare variables: modelName, brand, displaySize, price, hasHeartRateSensor
+ * Step 4: Assign the following data in main method:
+           Model: Series 9, 
+           Brand: Apple, 
+           Display: 1.92 inches, 
+           Price: 399.99 dollars, 
+           Heart Rate Sensor: true
+ * Step 5: Print the data in a professional format
+ */
