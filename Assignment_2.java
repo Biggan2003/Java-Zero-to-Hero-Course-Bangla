81d2910b848226ffaa84f5b50fc2cb7d5d9edcf8
G. M Biggan <gmbiggan@gmail.com>
2026-03-16 01:30:06 +0530

    Create Assignment_2.java
    Variable and Data Types

---
+++ Assignment_2.java
@@ -0,0 +1,14 @@
+/*
+ * Assignment-2 (Variable and Data type)
+ * Topic: Smart Library Management System
+ * * Step 1: Create a class called "Book"
+ * Step 2: Create a "main" method
+ * Step 3: Declare variables: bookId, bookName, authorName, price, isAvailable
+ * Step 4: Assign the following data in main method:
+           ID: 501, 
+           Title: Java Programming, 
+           Author: James Gosling, 
+           Price: 25.50 Euros, 
+           Availability: true
+ * Step 5: Print all the data with proper labels
+ */
